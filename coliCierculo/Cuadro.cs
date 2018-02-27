2e2ab0a245b535bc3ef6ac08d379e485c21c75b2
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-02-27 12:15:08 -0600

    agregando clase cuadro

---
+++ coliCierculo/Cuadro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace coliCierculo
+{
+    class Cuadro
+    {
+
+        public void Imprime(punto a, punto b)
+        {
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex2(a.x, a.y);
+            GL.Vertex2(b.x, a.y);
+            GL.Vertex2(b.x, b.y);
+            GL.Vertex2(a.x, b.y);
+            GL.End();
+        }
+    }
+}
